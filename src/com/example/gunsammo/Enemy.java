4eb68cf511ef7155c469cec2828cb067d0bc4b65
Ben Simpson <ben.simpson@edelman.com>
2013-11-22 22:24:11 +0000

    enemy class

---
+++ src/com/example/gunsammo/Enemy.java
@@ -0,0 +1,67 @@
+package com.example.gunsammo;
+
+public class Enemy {
+	private int maxHealth, currentHealth, power, speedX, centerX, centerY;
+
+	// Behavioral Methods
+	public void update() {
+		centerX += speedX;
+	}
+
+	public void die() {
+
+	}
+
+	public void attack() {
+
+	}
+
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public int getSpeedX() {
+		return speedX;
+	}
+
+	public int getCenterX() {
+		return centerX;
+	}
+
+	public int getCenterY() {
+		return centerY;
+	}
+
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+
+	public void setCurrentHealth(int currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+
+}
